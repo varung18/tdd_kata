7606f5068e91225c946c65b51f9ec6c69928e640
Varun Gaur <95574483+varung18@users.noreply.github.com>
2025-09-20 19:14:39 +0530

    Delete sweetshop/sweetshop directory

--- sweetshop/sweetshop/src/main/java/com/sweetshop/frontend/frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";               // import React as a namespace
-import * as ReactDOM from "react-dom/client"; // import ReactDOM as a namespace
-import App from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
